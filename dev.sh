05b669865b929a016f9b784d15d03d89eb61fab5
Shinichi Akiyama <shinichi.akiyama@gmail.com>
2017-10-20 13:22:51 +0900

    Fix forgetting to rename environment variable name

--- dev.sh
+++ dev.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 docker run -i -t --rm \
--e http_proxy="$http_proxy" \
--e https_proxy="$https_proxy" \
--e DB_ENV_MYSQL_USER=bbs \
--e DB_ENV_MYSQL_PASSWORD=bbs \
--e DB_ENV_MYSQL_DATABASE=bbs \
--e DB_PORT_3306_TCP_ADDR=bbs_db_1 \
--e DB_PORT_3306_TCP_PORT=3306 \
+-e http_proxy="${http_proxy:-}" \
+-e https_proxy="${https_proxy:-}" \
+-e DB_USER=bbs \
+-e DB_PASSWORD=bbs \
+-e DB_HOST=db \
+-e DB_PORT=3306 \
+-e DB_DATABASE=bbs \
 -v "$(pwd)":/usr/src/app \
 -w /usr/src/app \
 -p 4567:4567 \
