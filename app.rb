cbf2801009359c5fa3e9e49d7e71085390d71b74
Shinichi Akiyama <shinichi.akiyama@gmail.com>
2018-02-21 13:23:16 +0900

    Bump Bootstrap to 4

--- app.rb
+++ app.rb
@@ -38,17 +38,22 @@ __END__
 
 @@ index
 doctype html
-html lang="ja"
+html
   head
     meta charset="utf-8"
     title Sample BBS
-    link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+    link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"
   body
-    .container style="width: 730px;"
-      h1.page-header Sample BBS
+    nav.navbar.navbar-expand-lg.navbar-light.bg-light
+      .container
+        h1.navbar-brand Sample BBS
+        a href="https://github.com/shakiyam/bbs" GitHub
+    .container
+      br
       form method="post"
-         textarea.form-control name="body"
-         button.btn.btn-primary type="submit" Post
+        textarea.form-control name="body"
+        .d-flex.justify-content-end
+          button.btn.btn-primary type="submit" Post
       br
       .list-group
         - @posts.all.each do |post|
@@ -57,5 +62,8 @@ html lang="ja"
               = post[:body]
             footer.list-group-item-text.text-right.text-muted
               = post[:created_at]
-    script src="//code.jquery.com/jquery-3.2.1.min.js"
-    script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+      hr
+      | &copy; 2016 Shinichi Akiyama
+    script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"
+    script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"
+    script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"
