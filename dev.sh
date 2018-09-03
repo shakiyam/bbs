885ff5d567cb104a85432de22e88c240392e013d
Shinichi Akiyama <shinichi.akiyama@gmail.com>
2018-09-03 15:13:09 +0900

    Use name option

--- dev.sh
+++ dev.sh
@@ -1,17 +1,21 @@
-#!/bin/sh
+#!/bin/bash
+set -eu -o pipefail
+
 docker-compose up -d db
 ./build.sh
-docker run -i -t --rm \
--e http_proxy="${http_proxy:-}" \
--e https_proxy="${https_proxy:-}" \
--e DB_USER=bbs \
--e DB_PASSWORD=bbs \
--e DB_HOST=db \
--e DB_PORT=3306 \
--e DB_DATABASE=bbs \
--v "$(pwd)":/usr/src/app \
--w /usr/src/app \
--p 4567:4567 \
---net bbs_default \
---link bbs_db_1:db \
-shakiyam/bbs bash
+docker run \
+  -it \
+  --name bbs_development \
+  --net bbs_default \
+  --rm \
+  -p 4567:4567 \
+  -e http_proxy="${http_proxy:-}" \
+  -e https_proxy="${https_proxy:-}" \
+  -e DB_USER=bbs \
+  -e DB_PASSWORD=bbs \
+  -e DB_HOST=db \
+  -e DB_PORT=3306 \
+  -e DB_DATABASE=bbs \
+  -v "$(pwd)":/usr/src/app \
+  -u "$(id -u):$(id -g)" \
+  shakiyam/bbs bash
