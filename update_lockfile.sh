f06ad7988825150208242ea781c09284b51b517b
Shinichi Akiyama <shinichi.akiyama@gmail.com>
2018-09-03 15:10:45 +0900

    Change docker image

--- update_lockfile.sh
+++ update_lockfile.sh
@@ -1,7 +1,12 @@
-#!/bin/sh
-docker run --rm \
--e http_proxy="${http_proxy:-}" \
--e https_proxy="${https_proxy:-}" \
--v "$(pwd)":/usr/src/app \
--w /usr/src/app \
-jruby:9-alpine bundle lock --update
+#!/bin/bash
+set -eu -o pipefail
+
+docker container run \
+  --name bbs_update_lockfile \
+  --rm \
+  -e http_proxy="${http_proxy:-}" \
+  -e https_proxy="${https_proxy:-}" \
+  -v "$(pwd)":/usr/src/app \
+  -u "$(id -u):$(id -g)" \
+  -w /usr/src/app \
+  jruby:9 bundle lock --update
