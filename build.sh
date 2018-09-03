d01919e9925f307aecc2d3d5727613e8d164c086
Shinichi Akiyama <shinichi.akiyama@gmail.com>
2018-09-03 15:11:37 +0900

    Add tag

--- build.sh
+++ build.sh
@@ -1,2 +1,12 @@
-#!/bin/sh
-docker build --build-arg http_proxy="${http_proxy:-}" --build-arg https_proxy="${https_proxy:-}" -t shakiyam/bbs .
+#!/bin/bash
+set -eu -o pipefail
+
+current_image="$(docker image ls -q shakiyam/bbs:latest)"
+docker image build \
+  --build-arg http_proxy="${http_proxy:-}" \
+  --build-arg https_proxy="${https_proxy:-}" \
+  -t shakiyam/bbs .
+latest_image="$(docker image ls -q shakiyam/bbs:latest)"
+if [ "$current_image" != "$latest_image" ]; then
+  docker image tag shakiyam/bbs:latest shakiyam/bbs:"$(date +%Y%m%d%H%S)"
+fi
