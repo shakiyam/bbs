c63f03dfd35022dd043be5caf6f41e664a652ad4
Shinichi Akiyama <shinichi.akiyama@gmail.com>
2018-11-26 11:40:07 +0900

    Use readonly variable

--- build.sh
+++ build.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 set -eu -o pipefail
 
-current_image="$(docker image ls -q shakiyam/bbs:latest)"
+readonly IMAGE_NAME='shakiyam/bbs'
+
+current_image="$(docker image ls -q $IMAGE_NAME:latest)"
 docker image build \
   --build-arg http_proxy="${http_proxy:-}" \
   --build-arg https_proxy="${https_proxy:-}" \
-  -t shakiyam/bbs .
-latest_image="$(docker image ls -q shakiyam/bbs:latest)"
-if [ "$current_image" != "$latest_image" ]; then
-  docker image tag shakiyam/bbs:latest shakiyam/bbs:"$(date +%Y%m%d%H%S)"
+  -t "$IMAGE_NAME" .
+latest_image="$(docker image ls -q $IMAGE_NAME:latest)"
+if [[ "$current_image" != "$latest_image" ]]; then
+  docker image tag $IMAGE_NAME:latest $IMAGE_NAME:"$(date +%Y%m%d%H%S)"
 fi
