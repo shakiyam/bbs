58b5b6d77ac92ebed903aa94f9ac227da6261e70
Shinichi Akiyama <shinichi.akiyama@gmail.com>
2019-02-27 16:00:48 +0900

    Read password from environment variables

--- dev.sh
+++ dev.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 set -eu -o pipefail
 
+if [[ -e .env ]]; then
+  # shellcheck disable=SC1091
+  . .env
+fi
+
+if [[ -z "${MYSQL_ROOT_PASSWORD:-}" || -z "${MYSQL_USER:-}" || -z "${MYSQL_PASSWORD:-}" || -z "${MYSQL_DATABASE:-}" ]]; then
+  echo "Required environment variable not defined."
+  exit 1
+fi
+
 docker-compose up -d db
 ./build.sh
 docker run \
@@ -11,11 +21,11 @@ docker run \
   -p 4567:4567 \
   -e http_proxy="${http_proxy:-}" \
   -e https_proxy="${https_proxy:-}" \
-  -e DB_USER=bbs \
-  -e DB_PASSWORD=bbs \
+  -e DB_USER="${MYSQL_USER}" \
+  -e DB_PASSWORD="${MYSQL_PASSWORD}" \
   -e DB_HOST=db \
   -e DB_PORT=3306 \
-  -e DB_DATABASE=bbs \
+  -e DB_DATABASE="${MYSQL_DATABASE}" \
   -v "$(pwd)":/opt/bbs \
   -u "$(id -u):$(id -g)" \
   shakiyam/bbs bash
--- mysql.sh
+++ mysql.sh
@@ -1,4 +1,15 @@
-#!/bin/sh
+#!/bin/bash
+set -eu -o pipefail
+
+if [[ -e .env ]]; then
+  # shellcheck disable=SC1091
+  . .env
+fi
+
+if [[ -z "${MYSQL_ROOT_PASSWORD:-}" || -z "${MYSQL_USER:-}" || -z "${MYSQL_PASSWORD:-}" || -z "${MYSQL_DATABASE:-}" ]]; then
+  echo "Required environment variable not defined."
+  exit 1
+fi
+
 docker-compose up -d db
-docker exec -i -t bbs_db_1 \
-    mysql -u bbs -D bbs -pbbs --default-character-set=utf8mb4
+docker exec -it bbs_db_1 mysql -u "${MYSQL_USER}" -p"${MYSQL_PASSWORD}" "${MYSQL_DATABASE}"
