f444ef0f4ad901bb6bcef8de5ef7ab947460ffea
Shinichi Akiyama <shinichi.akiyama@gmail.com>
2021-01-18 19:07:12 +0900

    Declare and assign separately to avoid masking return values

--- remove_images.sh
+++ remove_images.sh
@@ -2,7 +2,8 @@
 set -eu -o pipefail
 
 readonly IMAGE_NAME='shakiyam/bbs'
-readonly LATEST_IMAGE="$(docker image ls -q $IMAGE_NAME:latest)"
+LATEST_IMAGE="$(docker image ls -q $IMAGE_NAME:latest)"
+readonly LATEST_IMAGE
 if [[ -n "$LATEST_IMAGE" ]]; then
   docker image rm -f "$LATEST_IMAGE"
 fi
