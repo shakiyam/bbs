ce87951aa7c875de6376c22a9481913fac47e540
Shinichi Akiyama <shinichi.akiyama@gmail.com>
2021-01-19 13:32:22 +0900

    Add container status check

--- wait-to-get-healthy.sh
+++ wait-to-get-healthy.sh
@@ -2,12 +2,21 @@
 set -eu -o pipefail
 
 if [[ $# -ne 1 ]]; then
-   echo -e "\033[36mContainer ID or NAME is required\033[0m"
-   exit 1
+  echo -e "\033[36mContainer ID or NAME is required\033[0m"
+  exit 1
 fi
 
 echo -n "Waiting for $1 to get healthy ..."
-until [[ "$(docker inspect -f '{{.State.Health.Status}}' "$1")" == "healthy" ]]; do
+while true
+do
+  status="$(docker inspect -f '{{.State.Status}}' "$1")"
+  if [[ $status != "running" ]]; then
+    echo -e "\n\033[36mContainer $1 is $status\033[0m"
+    exit 1
+  fi
+  if [[ "$(docker inspect -f '{{.State.Health.Status}}' "$1")" == "healthy" ]]; then
+    break
+  fi
   sleep 1
   echo -n .
 done
