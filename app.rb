7114f1a6965ceab091eaa19442754326557354c9
Shinichi Akiyama <shinichi.akiyama@gmail.com>
2021-05-11 12:40:20 +0900

    Bump Bootstrap from 4.6 to 5

--- app.rb
+++ app.rb
@@ -46,9 +46,10 @@ doctype html
 html
   head
     meta charset="utf-8"
+    meta name="viewport" content="width=device-width, initial-scale=1"
+    link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous"
     title Sample BBS
-    link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous"
-  body.d-flex.flex-column style="min-height: 100vh"
+  body.d-flex.flex-column.min-vh-100
     nav.navbar.navbar-expand-lg.navbar-light.bg-light
       .container
         h1.navbar-brand Sample BBS
@@ -65,9 +66,8 @@ html
           .list-group-item
             p
               = post[:body]
-            footer.text-right.text-muted
+            footer.text-end.text-muted
               = post[:created_at]
     footer.container
       | &copy; 2016 Shinichi Akiyama
-    script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"
-    script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"
+    script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-p34f1UUtsS3wqzfto5wAAmdvj+osOnFyQFpp4Ua3gs/ZVWx6oOypYoCJhGGScy+8" crossorigin="anonymous"
