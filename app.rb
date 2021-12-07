b04343284d0a715465f551788d43ad70a09d3b46
Shinichi Akiyama <shinichi.akiyama@gmail.com>
2021-12-07 12:45:23 +0900

    Minor layout change

--- app.rb
+++ app.rb
@@ -54,13 +54,11 @@ html
       .container
         h1.navbar-brand Sample BBS
         a href="https://github.com/shakiyam/bbs" GitHub
-    main.container.mb-auto
-      br
-      form method="post"
+    main.container.mb-auto.py-3
+      form.pb-5 method="post"
         textarea.form-control name="body"
         .d-flex.justify-content-end
           button.btn.btn-primary type="submit" Post
-      br
       .list-group
         - @posts.all.each do |post|
           .list-group-item
@@ -68,6 +66,6 @@ html
               = post[:body]
             footer.text-end.text-muted
               = post[:created_at]
-    footer.container
+    footer.container.text-center.py-3
       | &copy; 2016 Shinichi Akiyama
     script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"
