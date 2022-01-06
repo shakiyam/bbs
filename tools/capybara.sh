9a6c0070e6a82b5bc768d49df109248add93e449
Shinichi Akiyama <shinichi.akiyama@gmail.com>
2022-01-06 15:57:48 +0900

    Generalize tool scripts

--- tools/capybara.sh
+++ tools/capybara.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 set -eu -o pipefail
 
-docker container run \
-  --name capybara$$ \
-  --net bbs_default \
-  --rm \
-  -t \
-  -u "$(id -u):$(id -g)" \
-  -v "$PWD":/work:ro \
-  shakiyam/capybara "$@"
+if [[ $(command -v podman) ]]; then
+  podman container run \
+    --name capybara$$ \
+    --net "${NETWORK:-bridge}" \
+    --rm \
+    -t \
+    --security-opt label=disable \
+    -v "$PWD":/work:ro \
+    shakiyam/capybara "$@"
+else
+  docker container run \
+    --name capybara$$ \
+    --net "${NETWORK:-bridge}" \
+    --rm \
+    -t \
+    -u "$(id -u):$(id -g)" \
+    -v "$PWD":/work:ro \
+    shakiyam/capybara "$@"
+fi
