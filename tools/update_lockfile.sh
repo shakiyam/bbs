5eb90f5e617dbc6058495bacd29b5fd66ef803d3
Shinichi Akiyama <shinichi.akiyama@gmail.com>
2022-01-06 11:13:06 +0900

    Generalize tool scripts

--- tools/update_lockfile.sh
+++ tools/update_lockfile.sh
@@ -2,11 +2,22 @@
 set -eu -o pipefail
 
 [[ -e Gemfile.lock ]] || touch Gemfile.lock
-docker container run \
-  --name update_lockfile$$ \
-  --rm \
-  -u "$(id -u):$(id -g)" \
-  -v "$PWD/Gemfile":/work/Gemfile:ro \
-  -v "$PWD/Gemfile.lock":/work/Gemfile.lock \
-  -w /work \
-  ruby:alpine sh -c 'HOME=/tmp bundle lock --update'
+if [[ $(command -v podman) ]]; then
+  podman container run \
+    --name update_lockfile$$ \
+    --rm \
+    --security-opt label=disable \
+    -v "$PWD/Gemfile":/work/Gemfile:ro \
+    -v "$PWD/Gemfile.lock":/work/Gemfile.lock \
+    -w /work \
+    docker.io/ruby:alpine sh -c 'HOME=/tmp bundle lock --update'
+else
+  docker container run \
+    --name update_lockfile$$ \
+    --rm \
+    -u "$(id -u):$(id -g)" \
+    -v "$PWD/Gemfile":/work/Gemfile:ro \
+    -v "$PWD/Gemfile.lock":/work/Gemfile.lock \
+    -w /work \
+    ruby:alpine sh -c 'HOME=/tmp bundle lock --update'
+fi
