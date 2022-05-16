46e4be2dd0780e38c24adeb284690a532f6aa895
Shinichi Akiyama <shinichi.akiyama@gmail.com>
2022-05-16 10:26:14 +0900

    Change configuration file to required

--- tools/docker-compose-wrapper.sh
+++ tools/docker-compose-wrapper.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 set -eu -o pipefail
 
-if [[ -e .env ]]; then
-  # shellcheck disable=SC1091
-  . .env
+if [[ ! -e .env ]]; then
+  echo 'Environment file .env not found.'
+  exit 1
 fi
+# shellcheck disable=SC1091
+. .env
 
 if [[ -z "${MYSQL_ROOT_PASSWORD:-}" || -z "${MYSQL_USER:-}" || -z "${MYSQL_PASSWORD:-}" || -z "${MYSQL_DATABASE:-}" ]]; then
   echo "Required environment variable not defined."
