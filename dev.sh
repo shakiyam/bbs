e18aef480f018d0017838de8825e26378dc5d591
Shinichi Akiyama <shinichi.akiyama@gmail.com>
2022-09-07 14:58:25 +0900

    Support podman and Docker Compose V2

--- dev.sh
+++ dev.sh
@@ -13,18 +13,36 @@ fi
 
 ./tools/docker-compose-wrapper.sh up -d db
 ./tools/build.sh docker.io/shakiyam/bbs
-docker container run \
-  --name bbs-development \
-  --net bbs-default \
-  --rm \
-  -e DB_DATABASE="${MYSQL_DATABASE}" \
-  -e DB_HOST=db \
-  -e DB_PASSWORD="${MYSQL_PASSWORD}" \
-  -e DB_PORT=3306 \
-  -e DB_USER="${MYSQL_USER}" \
-  -e HOME=/tmp \
-  -it \
-  -p 4567:4567 \
-  -u "$(id -u):$(id -g)" \
-  -v "$(pwd)":/opt/bbs \
-  docker.io/shakiyam/bbs sh
+if [[ $(command -v docker) ]]; then
+  docker container run \
+    --name bbs-development \
+    --net bbs-default \
+    --rm \
+    -e DB_DATABASE="${MYSQL_DATABASE}" \
+    -e DB_HOST=db \
+    -e DB_PASSWORD="${MYSQL_PASSWORD}" \
+    -e DB_PORT=3306 \
+    -e DB_USER="${MYSQL_USER}" \
+    -e HOME=/tmp \
+    -it \
+    -p 4567:4567 \
+    -u "$(id -u):$(id -g)" \
+    -v "$PWD":/opt/bbs \
+    docker.io/shakiyam/bbs sh
+else
+  podman container run \
+    --name bbs-development \
+    --net bbs-default \
+    --rm \
+    --security-opt label=disable \
+    -e DB_DATABASE="${MYSQL_DATABASE}" \
+    -e DB_HOST=db \
+    -e DB_PASSWORD="${MYSQL_PASSWORD}" \
+    -e DB_PORT=3306 \
+    -e DB_USER="${MYSQL_USER}" \
+    -e HOME=/tmp \
+    -it \
+    -p 4567:4567 \
+    -v "$PWD":/opt/bbs \
+    docker.io/shakiyam/bbs sh
+fi
--- mysql.sh
+++ mysql.sh
@@ -11,7 +11,7 @@ if [[ -z "${MYSQL_ROOT_PASSWORD:-}" || -z "${MYSQL_USER:-}" || -z "${MYSQL_PASSW
   exit 1
 fi
 
-docker-compose up -d db
+./tools/docker-compose-wrapper.sh up -d db
 DOCKER=$(command -v docker || command -v podman)
 readonly DOCKER
 $DOCKER exec -it bbs-db mysql --host db --port 3306 -u "${MYSQL_USER}" -p"${MYSQL_PASSWORD}" "${MYSQL_DATABASE}"
