b7159f56dd2489893c21235f69f2b5bcc9565161
Shinichi Akiyama <shinichi.akiyama@gmail.com>
2022-10-07 10:07:22 +0900

    Use colored_echo.sh

--- tools/docker-compose-wrapper.sh
+++ tools/docker-compose-wrapper.sh
@@ -1,20 +1,31 @@
 #!/bin/bash
 set -eu -o pipefail
 
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+readonly SCRIPT_DIR
+# shellcheck disable=SC1091
+. "$SCRIPT_DIR"/colored_echo.sh
+
+if [[ $(command -v docker-compose) ]]; then
+  DOCKER_COMPOSE=docker-compose
+elif command -v docker >/dev/null && docker compose version >/dev/null 2>&1; then
+  DOCKER_COMPOSE='docker compose'
+else
+  echo_error 'Docker Compose is not installed.'
+  exit 1
+fi
+readonly DOCKER_COMPOSE
+
 if [[ ! -e .env ]]; then
-  echo 'Environment file .env not found.'
+  echo_error 'Environment file .env not found.'
   exit 1
 fi
 # shellcheck disable=SC1091
 . .env
 
 if [[ -z "${MYSQL_ROOT_PASSWORD:-}" || -z "${MYSQL_USER:-}" || -z "${MYSQL_PASSWORD:-}" || -z "${MYSQL_DATABASE:-}" ]]; then
-  echo "Required environment variable not defined."
+  echo_error 'Required environment variable not defined.'
   exit 1
 fi
 
-if [[ $(command -v docker-compose) ]]; then
-  docker-compose "$@"
-else
-  docker compose "$@"
-fi
+$DOCKER_COMPOSE "$@"
