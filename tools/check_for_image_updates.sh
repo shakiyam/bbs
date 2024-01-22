44140a889df2b835284534afa389b43f1ba69dc7
Shinichi Akiyama <shinichi.akiyama@gmail.com>
2024-01-22 08:39:39 +0000

    Use regctl

--- tools/check_for_image_updates.sh
+++ tools/check_for_image_updates.sh
@@ -6,15 +6,26 @@ readonly SCRIPT_DIR
 # shellcheck disable=SC1091
 . "$SCRIPT_DIR"/colored_echo.sh
 
-if [[ $(command -v docker) ]]; then
-  DOCKER=docker
-elif [[ $(command -v podman) ]]; then
-  DOCKER=podman
-else
-  echo_error "Neither docker nor podman is installed."
-  exit 1
+if [[ ! -x "$SCRIPT_DIR"/regctl ]]; then
+  echo_warn "Required dependency 'regctl' is missing, download it."
+  LATEST=$(
+    curl -sSI https://github.com/regclient/regclient/releases/latest \
+      | tr -d '\r' \
+      | awk -F'/' '/^[Ll]ocation:/{print $NF}'
+  )
+  readonly LATEST
+  case $(uname -m) in
+    x86_64)
+      ARCHITECTURE=amd64
+      ;;
+    aarch64)
+      ARCHITECTURE=arm64
+      ;;
+  esac
+  curl -L# "https://github.com/regclient/regclient/releases/download/${LATEST}/regctl-linux-${ARCHITECTURE}" \
+    >"$SCRIPT_DIR"/regctl
+  chmod +x "$SCRIPT_DIR"/regctl
 fi
-readonly DOCKER
 
 if [[ $# -ne 2 ]]; then
   echo_error "Usage: check_for_image_updates.sh image_name1 image_name2"
@@ -24,13 +35,11 @@ readonly IMAGE_NAME1=$1
 readonly IMAGE_NAME2=$2
 
 echo_info "$IMAGE_NAME1 and $IMAGE_NAME2"
-$DOCKER image pull -q "$IMAGE_NAME1"
-IMAGE_ID1="$($DOCKER image inspect -f "{{.Id}}" "$IMAGE_NAME1")"
-readonly IMAGE_ID1
-$DOCKER image pull -q "$IMAGE_NAME2"
-IMAGE_ID2="$($DOCKER image inspect -f "{{.Id}}" "$IMAGE_NAME2")"
-readonly IMAGE_ID2
-if [[ "$IMAGE_ID1" != "$IMAGE_ID2" ]]; then
+IMAGE_DIGEST1="$("$SCRIPT_DIR"/regctl manifest digest "$IMAGE_NAME1")"
+readonly IMAGE_DIGEST1
+IMAGE_DIGEST2="$("$SCRIPT_DIR"/regctl manifest digest "$IMAGE_NAME2")"
+readonly IMAGE_DIGEST2
+if [[ "$IMAGE_DIGEST1" != "$IMAGE_DIGEST2" ]]; then
   echo_warn "$IMAGE_NAME1 and $IMAGE_NAME2 are not the same."
   exit 2
 fi
