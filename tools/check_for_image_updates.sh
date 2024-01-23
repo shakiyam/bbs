350e0d8aa8bb5dd2abda9411f23a87146e0b5144
Shinichi Akiyama <shinichi.akiyama@gmail.com>
2024-01-23 05:38:37 +0000

    Use regctl if it's already installed

--- tools/check_for_image_updates.sh
+++ tools/check_for_image_updates.sh
@@ -6,26 +6,32 @@ readonly SCRIPT_DIR
 # shellcheck disable=SC1091
 . "$SCRIPT_DIR"/colored_echo.sh
 
-if [[ ! -x "$SCRIPT_DIR"/regctl ]]; then
-  echo_warn "Required dependency 'regctl' is missing, download it."
-  LATEST=$(
-    curl -sSI https://github.com/regclient/regclient/releases/latest \
-      | tr -d '\r' \
-      | awk -F'/' '/^[Ll]ocation:/{print $NF}'
-  )
-  readonly LATEST
-  case $(uname -m) in
-    x86_64)
-      ARCHITECTURE=amd64
-      ;;
-    aarch64)
-      ARCHITECTURE=arm64
-      ;;
-  esac
-  curl -L# "https://github.com/regclient/regclient/releases/download/${LATEST}/regctl-linux-${ARCHITECTURE}" \
-    >"$SCRIPT_DIR"/regctl
-  chmod +x "$SCRIPT_DIR"/regctl
+if [[ $(command -v regctl) ]]; then
+  REGCTL=regctl
+else
+  REGCTL="$SCRIPT_DIR"/regctl
+  if [[ ! -x "$REGCTL" ]]; then
+    echo_warn "Required dependency 'regctl' is missing, download it."
+    LATEST=$(
+      curl -sSI https://github.com/regclient/regclient/releases/latest \
+        | tr -d '\r' \
+        | awk -F'/' '/^[Ll]ocation:/{print $NF}'
+    )
+    readonly LATEST
+    case $(uname -m) in
+      x86_64)
+        ARCHITECTURE=amd64
+        ;;
+      aarch64)
+        ARCHITECTURE=arm64
+        ;;
+    esac
+    readonly ARCHITECTURE
+    curl -L# "https://github.com/regclient/regclient/releases/download/${LATEST}/regctl-linux-${ARCHITECTURE}" >"$REGCTL"
+    chmod +x "$REGCTL"
+  fi
 fi
+readonly REGCTL
 
 if [[ $# -ne 2 ]]; then
   echo_error "Usage: check_for_image_updates.sh image_name1 image_name2"
@@ -35,9 +41,9 @@ readonly IMAGE_NAME1=$1
 readonly IMAGE_NAME2=$2
 
 echo_info "$IMAGE_NAME1 and $IMAGE_NAME2"
-IMAGE_DIGEST1="$("$SCRIPT_DIR"/regctl manifest digest "$IMAGE_NAME1")"
+IMAGE_DIGEST1="$("$REGCTL" manifest digest "$IMAGE_NAME1")"
 readonly IMAGE_DIGEST1
-IMAGE_DIGEST2="$("$SCRIPT_DIR"/regctl manifest digest "$IMAGE_NAME2")"
+IMAGE_DIGEST2="$("$REGCTL" manifest digest "$IMAGE_NAME2")"
 readonly IMAGE_DIGEST2
 if [[ "$IMAGE_DIGEST1" != "$IMAGE_DIGEST2" ]]; then
   echo_warn "$IMAGE_NAME1 and $IMAGE_NAME2 are not the same."
