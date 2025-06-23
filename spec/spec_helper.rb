c49e48f3f44556410d92bd39ae74af5c12d9467a
Shinichi Akiyama <shinichi.akiyama@gmail.com>
2025-06-23 04:28:43 +0000

    Fix Capybara app_host URL format

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,5 +5,5 @@ Capybara.app = proc do
   ['200', { 'Content-Type' => 'text/html' }, ['This is a dummy app.']]
 end
 Capybara.default_driver = :mechanize
-Capybara.app_host = 'http://localhost:4567/'
+Capybara.app_host = 'http://localhost:4567'
 Capybara.run_server = false
