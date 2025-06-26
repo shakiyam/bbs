938c9feab4409fcae5061ffa03a9d6aba073ce52
Shinichi Akiyama <shinichi.akiyama@gmail.com>
2025-06-26 04:56:43 +0000

    Add stdin support to mysql.sh for piped SQL commands

--- mysql.sh
+++ mysql.sh
@@ -14,5 +14,13 @@ if [[ -z "$($DOCKER ps --filter "name=^bbs-db$" --filter "status=running" --quie
   exit 1
 fi
 
+if [[ -t 0 ]]; then
+  TTY_OPTION='-t'
+else
+  TTY_OPTION=''
+fi
+readonly TTY_OPTION
+
 # shellcheck disable=SC2016
-$DOCKER exec -it bbs-db sh -c 'MYSQL_PWD=$MYSQL_PASSWORD mysql --host=bbs-db --port=3306 --database=$MYSQL_DATABASE --user=$MYSQL_USER --default-character-set=utf8mb4'
+MYSQL_CMD='MYSQL_PWD=$MYSQL_PASSWORD mysql --host=bbs-db --port=3306 --database=$MYSQL_DATABASE --user=$MYSQL_USER --default-character-set=utf8mb4'
+$DOCKER exec -i $TTY_OPTION bbs-db sh -c "$MYSQL_CMD"
