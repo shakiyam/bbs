25615cb6758a877b2f2b9be0b5b702b56ee7e5b5
Shinichi Akiyama <shinichi.akiyama@gmail.com>
2025-09-10 07:48:00 +0000

    Update dockerfmt.sh to download binary directly instead of using containers for better performance and consistency

--- tools/dockerfmt.sh
+++ tools/dockerfmt.sh
@@ -7,26 +7,35 @@ readonly SCRIPT_DIR
 . "$SCRIPT_DIR"/colored_echo.sh
 
 if command -v dockerfmt &>/dev/null; then
-  dockerfmt "$@"
-elif command -v docker &>/dev/null; then
-  docker container run \
-    --name dockerfmt$$ \
-    --rm \
-    --platform linux/amd64 \
-    -u "$(id -u):$(id -g)" \
-    -v "$PWD":/work \
-    -w /work \
-    ghcr.io/reteps/dockerfmt:latest "$@"
-elif command -v podman &>/dev/null; then
-  podman container run \
-    --name dockerfmt$$ \
-    --rm \
-    --platform linux/amd64 \
-    --security-opt label=disable \
-    -v "$PWD":/work \
-    -w /work \
-    ghcr.io/reteps/dockerfmt:latest "$@"
+  DOCKERFMT=dockerfmt
 else
-  echo_error 'dockerfmt could not be executed.'
-  exit 1
+  DOCKERFMT="$SCRIPT_DIR"/dockerfmt
+  if [[ ! -x "$DOCKERFMT" ]]; then
+    echo_warn "Required dependency 'dockerfmt' is missing, download it."
+    case $(uname -m) in
+      x86_64)
+        ARCHITECTURE=amd64
+        ;;
+      aarch64)
+        ARCHITECTURE=arm64
+        ;;
+      *)
+        echo_error "Error: Unsupported architecture: $(uname -m)"
+        exit 1
+        ;;
+    esac
+    readonly ARCHITECTURE
+    LATEST=$(
+      curl -sSI https://github.com/reteps/dockerfmt/releases/latest \
+        | tr -d '\r' \
+        | awk -F'/' '/^[Ll]ocation:/{print $NF}'
+    )
+    readonly LATEST
+    curl -fL# "https://github.com/reteps/dockerfmt/releases/download/${LATEST}/dockerfmt-${LATEST}-linux-${ARCHITECTURE}.tar.gz" \
+      | tar xzf - -O dockerfmt \
+      | install -m 755 /dev/stdin "$DOCKERFMT"
+  fi
 fi
+readonly DOCKERFMT
+
+"$DOCKERFMT" "$@"
