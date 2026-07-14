d68faa41a502685c25d9158f0cfed751cfc0bd75
Shinichi Akiyama <shinichi.akiyama@gmail.com>
2026-07-14 02:31:36 +0000

    Skip backup when database container is not running

--- backup.sh
+++ backup.sh
@@ -8,11 +8,17 @@ readonly SCRIPT_DIR
 # shellcheck disable=SC1091
 . "$SCRIPT_DIR"/tools/container_engine.sh
 
+CONTAINER_ENGINE=$(detect_container_engine)
+readonly CONTAINER_ENGINE
+
+if [[ -z "$($CONTAINER_ENGINE ps --filter "name=^bbs-db$" --filter "status=running" --quiet)" ]]; then
+  echo_error 'bbs-db container is not running'
+  exit 1
+fi
+
 BACKUP_DIR="$SCRIPT_DIR/backup/$(date +%Y%m%d%H%M%S)"
 readonly BACKUP_DIR
 mkdir -p "$BACKUP_DIR"
-CONTAINER_ENGINE=$(detect_container_engine)
-readonly CONTAINER_ENGINE
 
 # shellcheck disable=SC2016
 MYSQLDUMP_CMD='MYSQL_PWD=$MYSQL_ROOT_PASSWORD mysqldump --databases $MYSQL_DATABASE --user=root'
